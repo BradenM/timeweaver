29093a355ccd17c70a95a9901dd91a6a43e46acb
Braden Mars <bradenmars@bradenmars.me>
2021-04-17 04:42:16 -0500

    feat(taskw): add dataclasses for parsing taskwarrior data

---
+++ twtw/taskw.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""Taskwarrior module."""
+
+import json
+from dataclasses import dataclass
+import subprocess as sp
+from typing import List
+
+
+@dataclass
+class TaskWarriorEntry:
+    id: int
+    description: str
+    project: str
+    tags: List[str]
+    uuid: str
+
+
+@dataclass
+class TaskWarriorData:
+    entries: List[TaskWarriorEntry]
+    
+    def __init__(self):
+        proc = sp.run(["/usr/bin/task", "export"], stdout=sp.PIPE, text=True)
+        lines = proc.stdout
+        data = json.loads(lines)
+        entries = []
+        for raw_e in data:
+            _fields = {k:raw_e.get(k) for k in TaskWarriorEntry.__dataclass_fields__}
+            entries.append(TaskWarriorEntry(**_fields))
+        self.entries = entries
+
+    @property
+    def projects(self):
+        return set([e.project for e in self.entries if e.project])
