73fc79aaa6119be18594d52d19fb1bf707907b22
Braden Mars <bradenmars@bradenmars.me>
2021-08-23 02:13:15 -0500

    feat(config): update config, add profile, tw host, other

--- twtw/models/config.py
+++ twtw/models/config.py
@@ -1,23 +1,32 @@
+from __future__ import annotations
+
 from typing import Optional
 
 from git import GitConfigParser
+from pydantic import HttpUrl, validator
+from tinydb import Query
+from tinydb.queries import QueryLike
 
-from twtw.models.base import TableModel, get_existing_value
+from twtw.models.base import TableModel
 
 
 class Config(TableModel):
+    PROFILE: str = "default"
+    TEAMWORK_HOST: Optional[HttpUrl] = None
     API_KEY: Optional[str] = None
     GIT_USER: Optional[str] = None
 
-    @classmethod
-    def get_current_git_user(cls) -> Optional[str]:
+    def query(self) -> QueryLike:
+        return Query().PROFILE == self.PROFILE
+
+    @validator("GIT_USER", pre=True, always=True)
+    def get_current_git_user(cls, v: Optional[str]) -> Optional[str]:
+        if v:
+            return v
         git_config = GitConfigParser(read_only=True)
         git_config.read()
         if git_config.has_option("user", "email"):
             return git_config.get("user", "email")
 
-    _validate_api_key = get_existing_value("API_KEY")
-    _validate_git_user = get_existing_value("GIT_USER", get_current_git_user)
 
-
-config = Config()
+config = Config().load()
