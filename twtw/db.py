9c8febaab54518403330743593ade8584ac0085e
Braden Mars <bradenmars@bradenmars.me>
2021-08-23 02:10:55 -0500

    feat(db): add `TableState` class as central source for db

--- twtw/db.py
+++ twtw/db.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+import attr
+from click import get_app_dir
+from tinydb import TinyDB
 from tinydb.storages import JSONStorage
 from tinydb_serialization import SerializationMiddleware, Serializer
 from tinydb_serialization.serializers import DateTimeSerializer
@@ -20,3 +23,30 @@ def create_db_storage() -> SerializationMiddleware:
     storage.register_serializer(PathSerializer(), "TinyPath")
     storage.register_serializer(DateTimeSerializer(), "TinyDate")
     return storage
+
+
+def _create_db(db_path: Path) -> TinyDB:
+    storage = create_db_storage()
+    return TinyDB(str(db_path), storage=storage, sort_keys=True, indent=4, separators=(",", ": "))
+
+
+def _on_update_path(instance: "_TableState", attrib: attr.Attribute, new_value: Path) -> Path:
+    new_value.parent.mkdir(exist_ok=True)
+    instance.db = _create_db(new_value)
+    return new_value
+
+
+@attr.s(auto_attribs=True, collect_by_mro=True)
+class _TableState:
+    db_path: Path = attr.ib(
+        factory=lambda: Path(get_app_dir("twtw")) / "db.json", on_setattr=_on_update_path
+    )
+    db: TinyDB = attr.ib()
+
+    @db.default
+    def setup_db(self) -> TinyDB:
+        self.db_path.parent.mkdir(exist_ok=True)
+        return _create_db(self.db_path)
+
+
+TableState = _TableState()
