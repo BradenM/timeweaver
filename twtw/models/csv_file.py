1efa15dc2981c09638d888b65da9b76ad1df45c6
Braden Mars <bradenmars@bradenmars.me>
2022-08-03 04:04:14 -0500

    feat(models): add concrete CSVRawEntry+CSVEntryLoader impls

---
+++ twtw/models/csv_file.py
@@ -0,0 +1,49 @@
+import csv
+from pathlib import Path
+from typing import Iterator, TypedDict
+
+import attrs
+from dateutil import parser as dparser
+
+from twtw.models.abc import EntryLoader, RawEntry
+
+CSVRawData = TypedDict(
+    "CSVRawData",
+    {"Activity type": str, "Duration": str, "From": str, "Comment": str, "id": str | None},
+    total=False,
+)
+
+
+@attrs.define
+class CSVRawEntry(RawEntry):
+    @property
+    def is_logged(self) -> bool:
+        return False
+
+
+@attrs.define
+class CSVEntryLoader(EntryLoader):
+    def load(self, csv_path: str, *args, **kwargs) -> Iterator[CSVRawData]:
+        if not csv_path:
+            raise TypeError(
+                f"CSVEntryLoader.load expected `csv_path` arg of type str, received: {type(csv_path)}."
+            )
+        csv_path = Path(csv_path).absolute()
+        if not csv_path.exists() or not csv_path.is_file():
+            raise FileNotFoundError(f"Path ({csv_path}) either does not exist or is not a file.")
+        with csv_path.open(newline="") as rows:
+            reader: Iterator[CSVRawData] = csv.DictReader(rows)
+            for idx, row in enumerate(reader):
+                if row.get("To", None) is not None and "Percent" not in row:
+                    yield row | {"id": idx}
+
+    def process(self, data: CSVRawData, *args, **kwargs) -> CSVRawEntry | None:
+        start = dparser.parse(data["From"]).astimezone()
+        end = dparser.parse(data["To"]).astimezone()
+        return CSVRawEntry(
+            id=data["id"],
+            tags=[data["Activity type"]],
+            start=start,
+            end=end,
+            annotation=data["Comment"],
+        )
