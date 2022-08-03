0ff3878f1632c3c7dd4fd584ae353fb6b9084487
Braden Mars <bradenmars@bradenmars.me>
2022-08-03 00:05:19 -0500

    feat(models): add teamwork uid config option + rich protocol

--- twtw/models/config.py
+++ twtw/models/config.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 from git import GitConfigParser
 from pydantic import HttpUrl, validator
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.table import Table
 from tinydb import Query
 from tinydb.queries import QueryLike
 
@@ -15,6 +17,7 @@ class Config(TableModel):
     TEAMWORK_HOST: Optional[HttpUrl] = None
     API_KEY: Optional[str] = None
     GIT_USER: Optional[str] = None
+    TEAMWORK_UID: Optional[str] = None
 
     def query(self) -> QueryLike:
         return Query().PROFILE == self.PROFILE
@@ -27,6 +30,13 @@ class Config(TableModel):
         git_config.read()
         if git_config.has_option("user", "email"):
             return git_config.get("user", "email")
+
+    def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        yield f"[b]{self.__class__.__name__}[/b]"
+        dt_table = Table("Field", "Value", expand=True, highlight=True)
+        for field, value in self.dict().items():
+            dt_table.add_row(field, str(value))
+        yield dt_table
 
 
 config = Config().load()
