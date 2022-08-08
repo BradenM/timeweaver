4ed81c151ae2756b994a331e8d95df75f26702b4
Braden Mars <bradenmars@bradenmars.me>
2022-08-08 03:27:12 -0500

    feat(state): handle project on per-entry level, add confirmation, improve reporting, more

--- twtw/state/entry.py
+++ twtw/state/entry.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import abc
 import enum
 import inspect
-import sys
 from enum import auto, unique
 from functools import partialmethod
 from inspect import Parameter
@@ -12,30 +11,33 @@ from typing import Callable, Iterator, ParamSpec, TypeVar
 
 import attrs
 import questionary
-from rich.columns import Columns
+from rich import box
+from rich.align import Align
 from rich.panel import Panel
+from rich.table import Table
+from rich.text import Text
 from transitions import EventData, Machine
 
 from twtw.api import Reporter
 from twtw.api.teamwork import TeamworkApi
+from twtw.db import TableState
 from twtw.models.abc import EntriesSource, RawEntry
 from twtw.models.csv_file import CSVEntryLoader
+from twtw.models.intervals import IntervalAggregator
 from twtw.models.models import (
     LogEntry,
     Project,
-    TeamworkProject,
     TeamworkTimeEntryRequest,
     TeamworkTimeEntryResponse,
 )
 
 T = TypeVar("T")
-P = ParamSpec("T")
+P = ParamSpec("P")
 
 
 @unique
 class CreateFlowState(enum.Enum):
     INIT = auto()
-    PROJECT = auto()
     ENTRIES = auto()
     REPOS = auto()
     COMMITS = auto()
@@ -71,6 +73,11 @@ class EntryContext:
     flags: FlowModifier = attrs.field(default=FlowModifier.ENTRIES_AVAILABLE)
     models: list[EntryFlowModel] = attrs.field(factory=list)
 
+    @property
+    def projects(self) -> list[Project]:
+        entries = TableState.db.table(Project.__name__).all()
+        return [Project.parse_obj(e).load() for e in entries]
+
     def create_model(self, raw_entry: RawEntry, flags: FlowModifier = None) -> EntryFlowModel:
         model = EntryFlowModel(
             context=self, raw_entry=raw_entry, model_flags=self.flags | (flags or self.flags)
@@ -79,8 +86,8 @@ class EntryContext:
         return model
 
 
-def unwrap_event(inst: type, event: EventData, *, f: Callable[P, T]) -> T:
-    event_kwargs: P.kwargs = event.kwargs
+def _unwrap_event(inst: type, event: EventData, *, f: Callable[P, T]) -> T:
+    event_kwargs = event.kwargs
     sig = inspect.signature(f)
     params = sig.parameters.copy()
     params.pop("self")
@@ -98,10 +105,10 @@ def unwrap_event(inst: type, event: EventData, *, f: Callable[P, T]) -> T:
 class EntryFlowModel:
     context: EntryContext
     raw_entry: RawEntry
+    description: str = attrs.field()
     model_flags: FlowModifier = attrs.field(
         default=FlowModifier.HAS_ENTRIES, on_setattr=_union_model_context_flags
     )
-    description: str = attrs.field()
     log_entry: LogEntry = attrs.field(default=None)
     teamw_api: TeamworkApi = attrs.field(factory=TeamworkApi)
     teamw_response: TeamworkTimeEntryResponse = attrs.field(default=None)
@@ -111,6 +118,14 @@ class EntryFlowModel:
         return self.raw_entry.description
 
     @property
+    def project(self) -> Project | None:
+        return next((p for p in self.context.projects if self.raw_entry.is_project(p) if p), None)
+
+    @property
+    def has_project(self) -> bool:
+        return self.project is not None
+
+    @property
     def flags(self) -> FlowModifier:
         return self.context.flags | self.model_flags
 
@@ -118,24 +133,26 @@ class EntryFlowModel:
     def dry_run(self) -> bool:
         return bool(self.flags & FlowModifier.DRY_RUN)
 
-    def create_entry(self, *, project: Project) -> EntryFlowModel:
+    def create_entry(self, *args, **kwargs) -> EntryFlowModel:
         entry = LogEntry(
             time_entry=self.raw_entry,
-            project=project,
+            project=self.project,
             description=str(self.description or self.raw_entry.description),
         )
         self.log_entry = entry
         return self
 
-    def create_payload(self, *, teamwork_project: TeamworkProject) -> TeamworkTimeEntryRequest:
+    def create_payload(self) -> TeamworkTimeEntryRequest:
         payload = TeamworkTimeEntryRequest.from_entry(
             entry=self.log_entry, person_id=self.teamw_api.person_id
         )
         return payload
 
-    def commit_entry(self, *, teamwork_project: TeamworkProject):
-        payload = self.create_payload(teamwork_project=teamwork_project)
-        response = self.teamw_api.create_time_entry(teamwork_project.project_id, payload)
+    def commit_entry(self, *args, **kwargs):
+        payload = self.create_payload()
+        response = self.teamw_api.create_time_entry(
+            self.project.resolve_teamwork_project().project_id, payload
+        )
         if not response.status == "OK":
             raise RuntimeError(
                 f"Failed to post entry, teamwork responsed with: {response.status} ({response})"
@@ -147,7 +164,7 @@ class EntryFlowModel:
             self.log_entry.teamwork_id = self.teamw_response.time_log_id
         self.log_entry.save()
 
-    bound_unwrap = partialmethod(unwrap_event)
+    bound_unwrap = partialmethod(_unwrap_event)
     create_entry_handler = partialmethod(bound_unwrap, f=create_entry)
     commit_entry_handler = partialmethod(bound_unwrap, f=commit_entry)
     save_entry_handler = partialmethod(bound_unwrap, f=save_entry)
@@ -169,22 +186,16 @@ class AbstractEntryFlow(abc.ABC):
     def create_transitions(cls, machine: Machine) -> Machine:
         ...
 
-    # @abc.abstractmethod
-    # def after_state_change(self, event: EventData) -> Machine:
-    #     ...
-
     @classmethod
     def create_machine(cls, inst: AbstractEntryFlow) -> Machine:
         machine = Machine(states=CreateFlowState, initial=CreateFlowState.INIT, send_event=True)
-        machine = cls.create_transitions(machine)  # flow = cls(=context, machine=machine)
+        machine = cls.create_transitions(machine)
         machine.add_model(inst)
-        # flow.machine.add_model(model=flow, initial=CreateFlowState.INIT)
         return machine
 
 
 @attrs.define(slots=False)
 class AbstractCreateEntryFlow(AbstractEntryFlow):
-    selected_entries: list[RawEntry] = attrs.field(factory=list)
     entry_machine: Machine = attrs.field(default=None)
 
     @classmethod
@@ -192,14 +203,10 @@ class AbstractCreateEntryFlow(AbstractEntryFlow):
         machine.add_transition(
             trigger="start",
             source=CreateFlowState.INIT,
-            dest=CreateFlowState.PROJECT,
-            before="load_context",
-        )
-        machine.add_transition(
-            trigger="choose",
-            source=CreateFlowState.PROJECT,
             dest=CreateFlowState.ENTRIES,
+            prepare="load_context",
             before="prepare_entries",
+            after="choose_entries",
         )
         machine.add_transition(
             trigger="choose",
@@ -229,13 +236,14 @@ class AbstractCreateEntryFlow(AbstractEntryFlow):
             trigger="choose",
             source=[CreateFlowState.ENTRIES, CreateFlowState.REPOS, CreateFlowState.COMMITS],
             dest=CreateFlowState.DRAFT,
-            after="create_drafts",
+            before="create_drafts",
+            after="review_drafts",
         )
         machine.add_transition(
             trigger="choose",
             source=CreateFlowState.DRAFT,
             dest=CreateFlowState.SAVE,
-            before="review_drafts",
+            before="confirm_drafts",
             after="commit_drafts",
         )
         return machine
@@ -276,9 +284,8 @@ class AbstractCreateEntryFlow(AbstractEntryFlow):
         return bool(self.context.flags & FlowModifier.COMMITS_AVAILABLE)
 
     def do_cancel(self, event: EventData):
-        print("exiting!")
-        print(event)
-        raise RuntimeError("something")
+        err_msg = "Cancelled!" if not event.args else ", ".join(event.args)
+        self.reporter.console.print(f"[bold bright_red]{err_msg}")
 
     def iter_choices(
         self, objs: list[T], key: Callable[[T], str] = None
@@ -304,16 +311,41 @@ class CSVCreateEntryFlow(AbstractCreateEntryFlow):
     project: Project = attrs.field(default=None)
     path: Path = attrs.field(default=None)
 
-    def load_context(self, event: EventData) -> None:
-        print(event)
+    def load_context(self, event: EventData) -> None:  # noqa
         source = EntriesSource.from_loader(CSVEntryLoader, self.path)
         self.context: EntryContext = EntryContext(source=source)
         self.entry_machine = Machine(
             model=None,
-            states=["init", "draft", "published", "complete"],
+            states=["init", "invalid", "skipped", "draft", "published", "complete"],
             send_event=True,
             initial="init",
             transitions=[
+                {
+                    "trigger": "validate",
+                    "source": "*",
+                    "dest": "invalid",
+                    "unless": "has_project",
+                },
+                {
+                    "trigger": "validate",
+                    "source": "*",
+                    "dest": "=",
+                    "conditions": "has_project",
+                },
+                {"trigger": "skip", "source": "*", "dest": "skipped"},
+                {"trigger": "next", "source": "skipped", "dest": "="},
+                {
+                    "trigger": "next",
+                    "source": "init",
+                    "dest": "invalid",
+                    "unless": "has_project",
+                },
+                {
+                    "trigger": "next",
+                    "source": "invalid",
+                    "dest": "=",
+                    "unless": "has_project",
+                },
                 {
                     "trigger": "next",
                     "source": "init",
@@ -342,46 +374,102 @@ class CSVCreateEntryFlow(AbstractCreateEntryFlow):
             ],
         )
 
-    def prepare_entries(self, event: EventData):
-        targets = self.context.source.unlogged_by_project(
-            self.project.resolve_teamwork_project().name
-        )
-        results = self.reporter.prompt.multiselect(targets, title="Choose Time Entries.")
-        if not any(results):
-            return self.cancel("no entries.")
-        self.selected_entries = results
-        self.context.flags |= FlowModifier.ENTRIES_SELECTED
-        for raw_entry in results:
+    def prepare_entries(self, event: EventData):  # noqa
+        targets = self.context.source.unlogged_entries
+        for raw_entry in targets:
             model = self.context.create_model(raw_entry=raw_entry)
             self.entry_machine.add_model(model)
+        self.entry_machine.dispatch("validate")
 
-    def create_drafts(self, event: EventData):
-        self.entry_machine.dispatch(
-            "next", project=self.project, teamwork_project=self.project.resolve_teamwork_project()
+    def choose_entries(self, event: EventData):  # noqa
+        disabled_help = "No Project Found!"
+        results: list[EntryFlowModel] = self.reporter.prompt.multiselect(
+            self.context.models,
+            title="Choose Time Entries.",
+            key=lambda e: str(e.raw_entry),
+            disabled=lambda e: disabled_help if e.is_invalid() else None,
         )
+        if not any(results):
+            return self.cancel("no entries.")
+        self.reporter.console.clear()
+        for mod in self.entry_machine.models:
+            if not mod.is_invalid() and mod not in results:
+                mod.skip()
+        self.context.flags |= FlowModifier.ENTRIES_SELECTED
 
-    def review_drafts(self, event: EventData):
-        cols = Columns([Panel(m.log_entry) for m in self.context.models], equal=True, expand=True)
-        self.reporter.console.print(cols)
+    def create_drafts(self, event: EventData):  # noqa
+        self.entry_machine.dispatch("next")
 
-    def commit_drafts(self, event: EventData):
-        self.entry_machine.dispatch(
-            "next", project=self.project, teamwork_project=self.project.resolve_teamwork_project()
+    def review_drafts(self, event: EventData):  # noqa
+        table_width = round(self.reporter.console.width // 1.15)
+        table = Table(
+            show_footer=True,
+            show_header=True,
+            header_style="bold bright_white",
+            box=box.SIMPLE_HEAD,
+            width=table_width,
+            title="Overview",
         )
-        try:
-            self.entry_machine.dispatch(
-                "next",
-                project=self.project,
-                teamwork_project=self.project.resolve_teamwork_project(),
+        table.add_column("ID")
+        table.add_column("Project", no_wrap=True)
+        table.add_column("Date", no_wrap=True)
+        table.add_column("Description", no_wrap=True)
+        table.add_column(
+            "Time", Text.from_markup("[b]Log Total", justify="right"), no_wrap=True, justify="right"
+        )
+        table.add_column("Duration", no_wrap=True, justify="right")
+
+        time_aggr = IntervalAggregator()
+        for model in self.context.models:
+            proj_tags = ",".join(model.raw_entry.tags)
+            project_name = (
+                f"[bold]Unknown:[/b] {proj_tags}" if not model.has_project else model.project.name
             )
-        except Exception:
-            # todo: do properly
-            if not self.dry_run:
-                raise
+            style = "bright_green"
+            match model.state:
+                case "invalid":
+                    style = "red italic"
+                case "skipped":
+                    style = "bright_black italic"
+                case _:
+                    time_aggr = time_aggr.add(model.raw_entry.interval)
+            table.add_row(
+                str(model.raw_entry.id),
+                project_name,
+                model.raw_entry.interval.day,
+                model.raw_entry.truncated_annotation(table_width // 3),
+                model.raw_entry.interval.span,
+                model.raw_entry.interval.padded_duration,
+                style=style,
+            )
+        table.columns[5].footer = Text.from_markup(
+            f"[u bright_green]{time_aggr.duration}", justify="right"
+        )
+
+        self.reporter.console.print(
+            Align.center(
+                Panel(
+                    table,
+                    padding=(
+                        1,
+                        3,
+                    ),
+                )
+            )
+        )
+
+    def confirm_drafts(self, event: EventData):
+        if self.dry_run:
+            return
+        if not self.reporter.prompt.confirm("Commit valid entries?"):
+            self.cancel("Canceled by user.")
+
+    def commit_drafts(self, event: EventData):  # noqa
+        self.entry_machine.dispatch("next")
+        if self.dry_run:
             self.reporter.console.print(
                 "[bright_black][bold](DRY RUN)[/bold] Pass [bright_white bold]--commit[/bright_white bold] to submit logs."
             )
-            sys.exit(0)
-        else:
-            self.reporter.console.rule("[bright_green bold]Submissions:[/]")
-            self.review_drafts(event)
+            return
+        self.reporter.console.print(":stopwatch:  [bold bright_white]Committing Entries...")
+        self.entry_machine.dispatch("next")
