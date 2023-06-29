c2c97105c4eeb47de078acdd055c7b4e90646340
Braden Mars <bradenmars@bradenmars.me>
2023-06-29 18:22:26 -0500

    feat(api): print submission message when submitting to teamwork
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- twtw/api/teamwork.py
+++ twtw/api/teamwork.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import attrs
 import httpx
+from rich import print
 
 from twtw.models.config import config
 from twtw.models.models import TeamworkTimeEntryRequest, TeamworkTimeEntryResponse
@@ -21,6 +22,9 @@ class TeamworkApi:
         self, project_id: str, request: TeamworkTimeEntryRequest
     ) -> TeamworkTimeEntryResponse:
         uri = f"{self.base_url}/projects/{project_id}/time_entries.json"
+        print(
+            f"[b bright_black]Submitting entry: {request.time_entry.hours}:{request.time_entry.minutes} @ {request.time_entry.date} [{request.time_entry.tags}]"
+        )
         payload = request.json(by_alias=True)
         response = httpx.post(uri, headers=self.headers, content=payload)
         response.raise_for_status()
