62d6bd40b01be370b24bf4593a52e619e02d1462
Braden Mars <bradenmars@bradenmars.me>
2023-06-30 01:35:13 -0500

    feat(models): track table loaded state, reuse fetched table model
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- twtw/models/base.py
+++ twtw/models/base.py
@@ -1,7 +1,7 @@
-from typing import TYPE_CHECKING, Any, Union
+from typing import TYPE_CHECKING, Any, ClassVar, Union, cast
 
 from loguru import logger
-from pydantic import BaseModel, parse_obj_as
+from pydantic import BaseModel, PrivateAttr, parse_obj_as
 from tinydb import Query
 from tinydb.queries import QueryLike
 from tinydb.table import Table
@@ -10,21 +10,26 @@ if TYPE_CHECKING:
     from pydantic.typing import AbstractSetIntStr, DictStrAny, MappingIntStrAny
 
 
-class TableModel(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
+class TableModel(BaseModel, arbitrary_types_allowed=True):
+    __table: ClassVar[Table]
+    _loaded: bool = PrivateAttr(False)
+
+    @property
+    def is_loaded(self) -> bool:
+        """Indicates model was loaded from table."""
+        return self._loaded
 
     @property
     def table(self) -> Table:
-        from twtw.db import TableState
-
-        return TableState.db.table(self.__class__.__name__)
+        return self.__class__.table_of()
 
     @classmethod
     def table_of(cls) -> Table:
-        from twtw.db import TableState
+        if not hasattr(cls, "__table"):
+            from twtw.db import TableState
 
-        return TableState.db.table(cls.__name__)
+            cls.__table = TableState.db.table(cls.__name__)
+        return cast(Table, cls.__table)
 
     @property
     def field_defaults(self) -> dict[str, Any]:
@@ -54,14 +59,18 @@ class TableModel(BaseModel):
         return super().dict(**params)
 
     def query(self) -> QueryLike:
+        """Query fragment for model."""
         return Query().fragment(self.dict(exclude_unset=True))
 
     def save(self) -> None:
+        """Upsert model to table."""
         _data = self.dict()
         logger.debug("[b]{}[/]: upserting (data={})", self.__class__.__name__, _data)
         self.table.upsert(_data, cond=self.query())
+        self._loaded = True
 
     def load(self) -> "TableModel":
+        """Load model from table."""
         query = self.query()
         data = self.table.get(query)
         if data:
@@ -72,5 +81,6 @@ class TableModel(BaseModel):
                 data,
             )
             _loaded = parse_obj_as(self.__class__, data)
+            _loaded._loaded = True
             return _loaded
         return self
