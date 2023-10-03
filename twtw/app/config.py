46eb28dcd122928394a0bc938ace9a962e3ec204
Braden Mars <bradenmars@bradenmars.me>
2023-10-03 15:02:53 -0500

    fix(config): close database post config
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- twtw/app/config.py
+++ twtw/app/config.py
@@ -1,14 +1,21 @@
 import typer
 from rich import print
 
+from twtw.db import TableState
 from twtw.models.config import config as app_config
 
 app = typer.Typer()
 
 
 @app.callback()
-def config():  # noqa: F811
+def config(ctx: typer.Context):  # noqa: F811
     """Manage TWTW config."""
+    
+    def _close_db():
+        print("Closing database...")
+        TableState.db.close()
+
+    ctx.call_on_close(_close_db)
 
 
 @app.command(name="list")
