794f498cec4af60ed80d411ad4c18288c73d9b31
Braden Mars <bradenmars@bradenmars.me>
2025-01-27 03:37:24 -0600

    feat(models.data_types): implement pydantic + sqlalchemy git commit validators / type decorators
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- twtw/models/data_types.py
+++ twtw/models/data_types.py
@@ -22,3 +22,49 @@ class PathType(TypeDecorator):
             return Path(value)
         return value
 
+
+class GitCommit(git.Commit):
+    """A git commit object that can be used with Pydantic."""
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value):
+        try:
+            repo, commit = value.split("@")
+            repo = git.Repo(repo)
+            return repo.commit(commit)
+        except ValueError as e:
+            raise ValueError(f"Invalid git commit: {value}") from e
+
+    def json(self):
+        return f"{self.value.repo.working_dir!s}@{self.value!s}"
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({super().__repr__()})"
+
+
+class SQLGitCommit(TypeDecorator):
+    """SQLAlchemy type for storing git.Commit objects."""
+
+    impl = String
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if isinstance(value, git.Commit):
+            return f"{value.repo.working_dir!s}@{value!s}"
+        if isinstance(value, GitCommit):
+            return value.json()
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            return GitCommit.validate(value)
+        return value
+
