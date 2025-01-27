9451573e972eec548bbc1c4bce73367111aa86f5
Braden Mars <bradenmars@bradenmars.me>
2025-01-27 03:52:17 -0600

    feat(api.teamwork): rework around log entry inputs
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- twtw/api/teamwork.py
+++ twtw/api/teamwork.py
@@ -1,32 +1,65 @@
 from __future__ import annotations
 
+import logging
+
 import attrs
-import httpx
 from rich import print
 
 from twtw.models.config import config
-from twtw.models.models import TeamworkTimeEntryRequest, TeamworkTimeEntryResponse
+from twtw.models.models import LogEntry, TeamworkTimeEntryRequest, TeamworkTimeEntryResponse
+
+from ._api import HttpClient
+
+logger = logging.getLogger(__name__)
 
 
 @attrs.define
 class TeamworkApi:
-    base_url: str = attrs.field(default=config.TEAMWORK_HOST)
-    person_id: str = attrs.field(default=config.TEAMWORK_UID)
-    api_key: str = attrs.field(default=config.API_KEY)
+    base_url: str = attrs.Factory(lambda: config.TEAMWORK_HOST)
+    basic_auth: str = attrs.Factory(lambda: config.API_KEY)
+    person_id: str = attrs.Factory(lambda: config.TEAMWORK_UID)
+    client: HttpClient = attrs.Factory(
+        lambda s: HttpClient(
+            base_url=s.base_url,
+            headers={"Authorization": f"Basic {s.basic_auth}", "Content-Type": "application/json"},
+        ),
+        takes_self=True,
+    )
 
-    @property
-    def headers(self) -> dict[str, str]:
-        return {"Authorization": f"Basic {config.API_KEY}", "Content-Type": "application/json"}
+    def _validate_time_entry_response(self, response) -> TeamworkTimeEntryResponse:
+        response = TeamworkTimeEntryResponse.parse_obj(response.json())
+        if not response.status == "OK":
+            raise RuntimeError(
+                f"Failed to post entry, teamwork responded with: {response.status} ({response})"
+            )
+        return response
 
     def create_time_entry(
-        self, project_id: str, request: TeamworkTimeEntryRequest
+        self, *, log_entry: LogEntry, project_id: str | int, person_id: str | int | None = None
     ) -> TeamworkTimeEntryResponse:
-        uri = f"{self.base_url}/projects/{project_id}/time_entries.json"
-        print(
-            f"[b bright_black]Submitting entry: {request.time_entry.hours}:{request.time_entry.minutes} @ {request.time_entry.date} [{request.time_entry.tags}]"
+        """Create a new teamwork time entry."""
+        request = TeamworkTimeEntryRequest.from_entry(
+            entry=log_entry, person_id=person_id or self.person_id
         )
-        payload = request.json(by_alias=True)
-        response = httpx.post(uri, headers=self.headers, content=payload)
-        response.raise_for_status()
-        response_data = TeamworkTimeEntryResponse.parse_obj(response.json())
-        return response_data
+        print(
+            f"[b bright_black]Creating Time Entry: {request.time_entry.hours}:{request.time_entry.minutes} @ {request.time_entry.date} [{request.time_entry.tags}]"
+        )
+        endpoint = f"/projects/{project_id}/time_entries.json"
+        response = self.client.post(endpoint, content=request.json(by_alias=True))
+        return self._validate_time_entry_response(response)
+
+    def update_time_entry(
+        self, log_entry: LogEntry, person_id: str | int | None = None
+    ) -> TeamworkTimeEntryResponse:
+        """Update an existing teamwork time entry."""
+        request = TeamworkTimeEntryRequest.from_entry(
+            entry=log_entry, person_id=person_id or self.person_id
+        )
+        print(
+            f"[b bright_black]Updating Time Entry (id: {log_entry.teamwork_id}): {request.time_entry.hours}:{request.time_entry.minutes} @ {request.time_entry.date} [{request.time_entry.tags}]"
+        )
+        endpoint = f"/time_entries/{log_entry.teamwork_id}.json"
+        response = self.client.put(endpoint, content=request.json(by_alias=True))
+        return self._validate_time_entry_response(response).copy(
+            update={"time_log_id": log_entry.teamwork_id}
+        )
