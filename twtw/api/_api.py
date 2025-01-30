48feb36f264e2520fe5c37ea0bb65724dcc42e47
Braden Mars <bradenmars@bradenmars.me>
2025-01-30 15:31:16 -0600

    feat(api._api): support dict input for `get` params, log requests
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- twtw/api/_api.py
+++ twtw/api/_api.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from typing import Any, TypeVar
 
 import attrs
@@ -7,6 +8,8 @@ import httpx
 
 RequestT = TypeVar("RequestT")
 ResponseT = TypeVar("ResponseT")
+
+logger = logging.getLogger(__name__)
 
 
 @attrs.define
@@ -17,8 +20,10 @@ class HttpClient:
         lambda s: httpx.Client(base_url=s.base_url, headers=s.headers), takes_self=True
     )
 
-    def get(self, endpoint: str, *, params: httpx.QueryParams) -> httpx.Response:
+    def get(self, endpoint: str, *, params: httpx.QueryParams | dict[str, Any]) -> httpx.Response:
+        params = params if isinstance(params, httpx.QueryParams) else httpx.QueryParams(params)
         with self.client:
+            logging.info("GET %s (params=%s)", endpoint, params)
             response = self.client.get(endpoint, params=params)
             response.raise_for_status()
             return response
@@ -27,6 +32,7 @@ class HttpClient:
         self, endpoint: str, *, json: dict[str, Any] | None = None, content: Any | None = None
     ) -> httpx.Response:
         with self.client as client:
+            logging.info("POST %s (json=%s, content=%s)", endpoint, json, content)
             response = client.post(endpoint, json=json, content=content)
             response.raise_for_status()
             return response
@@ -35,6 +41,7 @@ class HttpClient:
         self, endpoint: str, *, json: dict[str, Any] | None = None, content: Any | None = None
     ) -> httpx.Response:
         with self.client as client:
+            logging.info("PUT %s (json=%s, content=%s)", endpoint, json, content)
             response = client.put(endpoint, json=json, content=content)
             response.raise_for_status()
             return response
